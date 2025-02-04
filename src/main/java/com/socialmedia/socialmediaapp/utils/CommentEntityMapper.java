18e5159122d218a6ef1be78938389238518318df
udhayaj735 <udhayaj735@gmail.com>
2025-02-04 11:07:27 -0600

    ModelMapper Implemented for post and Comment

--- src/main/java/com/socialmedia/socialmediaapp/utils/CommentEntityMapper.java
+++ src/main/java/com/socialmedia/socialmediaapp/utils/CommentEntityMapper.java
@@ -2,19 +2,27 @@ package com.socialmedia.socialmediaapp.utils;
 
 import com.socialmedia.socialmediaapp.dto.CommentDto;
 import com.socialmedia.socialmediaapp.entity.CommentEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentEntityMapper {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public CommentDto mapCommentEntityToCommentDto(final CommentEntity commentEntity) {
         if (commentEntity != null) {
-            CommentDto commentDto = new CommentDto();
+
+         /*   CommentDto commentDto = new CommentDto();
             commentDto.setId(commentEntity.getId());
             commentDto.setUserName(commentEntity.getUserName());
             commentDto.setEmail(commentEntity.getEmail());
             commentDto.setBody(commentEntity.getBody());
-            return commentDto;
+            return commentDto;*/
+
+            return this.modelMapper.map(commentEntity,CommentDto.class);
         }
         return null;
     }
@@ -23,11 +31,12 @@ public class CommentEntityMapper {
 
     public CommentEntity mapCommentDtoToCommentEntity(final CommentDto commentDto) {
         if (commentDto != null) {
-            CommentEntity commentEntity = new CommentEntity();
+          /*  CommentEntity commentEntity = new CommentEntity();
             commentEntity.setUserName(commentDto.getUserName());
             commentEntity.setEmail(commentDto.getEmail());
             commentEntity.setBody(commentDto.getBody());
-            return commentEntity;
+            return commentEntity;*/
+            return this.modelMapper.map(commentDto,CommentEntity.class);
         }
         return null;
     }
--- src/main/java/com/socialmedia/socialmediaapp/utils/PostEntityMapper.java
+++ src/main/java/com/socialmedia/socialmediaapp/utils/PostEntityMapper.java
@@ -2,30 +2,37 @@ package com.socialmedia.socialmediaapp.utils;
 
 import com.socialmedia.socialmediaapp.dto.PostDto;
 import com.socialmedia.socialmediaapp.entity.PostEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PostEntityMapper {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     /*Map or convert post entity to post Dto*/
     public PostDto mapPostEntityToPostDto(final PostEntity postEntity)
     {
-        PostDto postDto=new PostDto();
+        return this.modelMapper.map(postEntity,PostDto.class);
+       /* PostDto postDto=new PostDto();
         postDto.setId(postEntity.getId());
         postDto.setTitle(postEntity.getTitle());
         postDto.setDescription(postEntity.getDescription());
         postDto.setContent(postEntity.getContent());
-        return postDto;
+        return postDto;*/
     }
 
     /*map or covert postDto to post entity*/
 
     public PostEntity mapPostDtoToPostEntity(final PostDto postDto)
     {
-        PostEntity postEntity=new PostEntity();
+        /*PostEntity postEntity=new PostEntity();
         postEntity.setTitle(postDto.getTitle());
         postEntity.setDescription(postDto.getDescription());
         postEntity.setContent(postDto.getContent());
-        return postEntity;
+        return postEntity;*/
+        return this.modelMapper.map(postDto,PostEntity.class);
     }
 }
